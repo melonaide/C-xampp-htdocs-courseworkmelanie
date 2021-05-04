d441fe3681a699ced18b079efd5d2371999e7642
melonaide <74668991+melonaide@users.noreply.github.com>
2021-05-04 22:41:41 +0100

    Update viewAnimals.php

--- viewAnimals.php
+++ viewAnimals.php
@@ -17,7 +17,25 @@
   </tr>
 </table>
 
+<tr><th>name</th> <th >date_of_birth</th><th >description</th> <th >picture</th><th >available</th><th ></tr>
 
+<?php
+  try{
+    $rows = $db->query("SELECT * FROM animal_info");
+
+    foreach ($rows as $row) {
+      echo  "<tr><td >" . $row['name'] . "</td><td >" . $row['date_of_birth'] . "</td><td >" . $row['description'];
+      echo "</td><td >" . $row['picture'] . "</td><td >" . $row['available']. "</td></tr>\n";
+    }
+
+    echo "</table> <br>";
+  } catch (PDOException $ex){
+
+    echo "Sorry, a database error occurred when querying the animal records. Please try again.<br> ";
+    echo "Error details:". $ex->getMessage();
+  }
+
+?>
 
   </body>
 </html>
